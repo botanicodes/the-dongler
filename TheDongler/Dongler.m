9180cc0e2c7b64ce5a3442ecf43d043389996c5d
Joseph Rivard <jrivard@ostusa.com>
2015-11-18 15:54:18 -0500

    Moar

--- TheDongler/Dongler.m
+++ TheDongler/Dongler.m
@@ -10,4 +10,47 @@
 
 @implementation Dongler
 
+- (instancetype)init
+{
+    self = [super init];
+    if(!self) return self;
+    
+    return self;
+}
+
+#pragma mark - Class methods
+
++ (Dongler*)sharedDongler
+{
+    static Dongler* dongler;
+    
+    if(!dongler){ // dongler hasn't been allocated yet
+        dongler = [[Dongler alloc] init];
+    }
+    
+    return dongler;
+}
+
++ (Dongle *)connectedDongle
+{
+    return nil;
+}
+
++ (void)listDonglesWithCompletion:(DonglerListComplete)complete
+{
+    
+}
+
++ (void)connectToDongle:(Dongle*)dongle withCompletion:(DonglerConnectionStateComplete)complete
+{
+    
+}
+
+
++ (void)disconnectFromDongle:(Dongle*)dongle withCompletion:(DonglerConnectionStateComplete)complete
+{
+    
+}
+
+
 @end
